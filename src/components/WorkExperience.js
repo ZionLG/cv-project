a0ade3c88da995ec363b03fda4a0ae703aeb2480
LGGC <liran12369@gmail.com>
2022-10-04 16:04:28 +0300

    add new Work Experience function component

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,47 +1,46 @@
 import React from "react";
 
-export default class WorkExperiance extends React.Component {
-  render() {
-    const work = this.props.workInfo.map((obj) => {
-      return (
-        <div key={obj.id} data-id={obj.id} className="input-taker">
-          <input
-            type="text"
-            name="position"
-            placeholder="Position"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="company"
-            placeholder="Company"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="startDate"
-            placeholder="Start Date"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="endDate"
-            placeholder="End Date"
-            onChange={this.props.handleOnChange}
-          />
-          <button data-id={obj.id} onClick={this.props.deleteWork}>
-            Delete
-          </button>
-        </div>
-      );
-    });
-
+const WorkExperiance = (props) => {
+  const work = props.workInfo.map((obj) => {
     return (
-      <section>
-        <h2>Experiance</h2>
-        {work}
-        <button onClick={this.props.addWork}>Add</button>
-      </section>
+      <div key={obj.id} data-id={obj.id} className="input-taker">
+        <input
+          type="text"
+          name="position"
+          placeholder="Position"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="company"
+          placeholder="Company"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="startDate"
+          placeholder="Start Date"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="endDate"
+          placeholder="End Date"
+          onChange={props.handleOnChange}
+        />
+        <button data-id={obj.id} onClick={props.deleteWork}>
+          Delete
+        </button>
+      </div>
     );
-  }
-}
+  });
+  return (
+    <section>
+      <h2>Experiance</h2>
+      {work}
+      <button onClick={props.addWork}>Add</button>
+    </section>
+  );
+};
+
+export default WorkExperiance;
