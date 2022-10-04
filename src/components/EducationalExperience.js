25f86b22a7e01e5076b459030eeffaa207168bb7
LGGC <liran12369@gmail.com>
2022-10-04 16:54:13 +0300

    add new Eductational Experience function component

--- src/components/EducationalExperience.js
+++ src/components/EducationalExperience.js
@@ -1,53 +1,53 @@
 import React from "react";
 
-export default class EducationalExperiance extends React.Component {
-  render() {
-    const education = this.props.educationInfo.map((obj) => {
-      return (
-        <div key={obj.id} data-id={obj.id} className="input-taker">
-          <input
-            type="text"
-            name="subject"
-            placeholder="Subject"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="degree"
-            placeholder="Degree"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="university"
-            placeholder="University"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="startDate"
-            placeholder="Start Date"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="endDate"
-            placeholder="End Date"
-            onChange={this.props.handleOnChange}
-          />
-          <button data-id={obj.id} onClick={this.props.deleteEducation}>
-            Delete
-          </button>
-        </div>
-      );
-    });
-
+const EducationalExperiance = (props) => {
+  const education = props.educationInfo.map((obj) => {
     return (
-      <section>
-        <h2>Education</h2>
-        {education}
-        <button onClick={this.props.addEducation}>Add</button>
-      </section>
+      <div key={obj.id} data-id={obj.id} className="input-taker">
+        <input
+          type="text"
+          name="subject"
+          placeholder="Subject"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="degree"
+          placeholder="Degree"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="university"
+          placeholder="University"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="startDate"
+          placeholder="Start Date"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="endDate"
+          placeholder="End Date"
+          onChange={props.handleOnChange}
+        />
+        <button data-id={obj.id} onClick={props.deleteEducation}>
+          Delete
+        </button>
+      </div>
     );
-  }
-}
+  });
+
+  return (
+    <section>
+      <h2>Education</h2>
+      {education}
+      <button onClick={props.addEducation}>Add</button>
+    </section>
+  );
+};
+
+export default EducationalExperiance;
