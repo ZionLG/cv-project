38969e878f85da7a28486fdf8612229c0b6de9b3
LGGC <liran12369@gmail.com>
2022-10-04 16:08:01 +0300

    add new Personal detail function component

--- src/components/PersonalDetailEditor.js
+++ src/components/PersonalDetailEditor.js
@@ -1,49 +1,49 @@
 import React from "react";
 
-export default class PersonalDetailEditor extends React.Component {
-  render() {
-    return (
-      <section>
-        <h2>Personal Details</h2>
-        <div className="input-taker">
-          <input
-            type="text"
-            name="fullName"
-            placeholder="Full name"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="tel"
-            name="phoneNumber"
-            placeholder="Phone number"
-            onChange={this.props.handleOnChange}
-          />
-          <input
-            type="text"
-            name="address"
-            placeholder="Address"
-            onChange={this.props.handleOnChange}
-          />
-          <textarea
-            name="description"
-            rows={5}
-            placeholder="Description"
-            onChange={this.props.handleOnChange}
-          />
-        </div>
-      </section>
-    );
-  }
-}
+const PersonalDetailEditor = (props) => {
+  return (
+    <section>
+      <h2>Personal Details</h2>
+      <div className="input-taker">
+        <input
+          type="text"
+          name="fullName"
+          placeholder="Full name"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="tel"
+          name="phoneNumber"
+          placeholder="Phone number"
+          onChange={props.handleOnChange}
+        />
+        <input
+          type="text"
+          name="address"
+          placeholder="Address"
+          onChange={props.handleOnChange}
+        />
+        <textarea
+          name="description"
+          rows={5}
+          placeholder="Description"
+          onChange={props.handleOnChange}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default PersonalDetailEditor;
