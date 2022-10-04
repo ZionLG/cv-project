2936ce504b78bc260fbec4e7fd98123e3bbab6c5
LGGC <liran12369@gmail.com>
2022-10-04 17:10:18 +0300

    add new Preview function component

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -4,77 +4,77 @@ import { ImLocation } from "react-icons/im";
 import { MdEmail } from "react-icons/md";
 import uniqid from "uniqid";
 
-export default class Preview extends React.Component {
-  render() {
-    const { fullName, title, email, phoneNumber, address, description } =
-      this.props.personalInfo;
+const Preview = (props) => {
+  const { fullName, title, email, phoneNumber, address, description } =
+    props.personalInfo;
 
-    const educationInfo = this.props.educationInfo.map((obj) => {
-      return (
-        <div key={uniqid()} className="preview-section-item">
-          <div className="preview-section-time">
-            {obj.startDate} - {obj.endDate}
-          </div>
-          <div className="preview-section-info">
-            <span className="preview-subject">{obj.subject}</span>
-            <span>{obj.degree}</span>
-            <span>{obj.university}</span>
-          </div>
-        </div>
-      );
-    });
-
-    const workInfo = this.props.workInfo.map((obj) => {
-      return (
-        <div key={uniqid()} className="preview-section-item">
-          <div className="preview-section-time">
-            {obj.startDate} - {obj.endDate}
-          </div>
-          <div className="preview-section-info">
-            <span className="preview-position">{obj.position}</span>
-            <span>{obj.company}</span>
-          </div>
-        </div>
-      );
-    });
-
+  const educationInfo = props.educationInfo.map((obj) => {
     return (
-      <div className="preview">
-        <section>
-          <div className="preview-info">
-            <div>
-              <h1 className="preview-name">{fullName}</h1>
-              <span>{title}</span>
-            </div>
-            <div className="preview-contact">
-              <span className="contact-line">
-                <FaPhoneSquareAlt size={15} />
-                {phoneNumber}
-              </span>
-              <span className="contact-line">
-                <MdEmail size={15} />
-                {email}
-              </span>
-              <span className="contact-line">
-                <ImLocation size={15} />
-                {address}
-              </span>
-            </div>
-          </div>
-          <hr />
-          <section>{description}</section>
-        </section>
-        <section className="preview-section">
-          {educationInfo.length > 0 && (
-            <h2 className="preview-name">Education</h2>
-          )}
-          {educationInfo}
-        </section>
-        <section className="preview-section">
-          {workInfo.length > 0 && <h2 className="preview-name">Experiance</h2>}
-          {workInfo}
-        </section>
+      <div key={uniqid()} className="preview-section-item">
+        <div className="preview-section-time">
+          {obj.startDate} - {obj.endDate}
+        </div>
+        <div className="preview-section-info">
+          <span className="preview-subject">{obj.subject}</span>
+          <span>{obj.degree}</span>
+          <span>{obj.university}</span>
+        </div>
       </div>
     );
-  }
-}
+  });
+
+  const workInfo = props.workInfo.map((obj) => {
+    return (
+      <div key={uniqid()} className="preview-section-item">
+        <div className="preview-section-time">
+          {obj.startDate} - {obj.endDate}
+        </div>
+        <div className="preview-section-info">
+          <span className="preview-position">{obj.position}</span>
+          <span>{obj.company}</span>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="preview">
+      <section>
+        <div className="preview-info">
+          <div>
+            <h1 className="preview-name">{fullName}</h1>
+            <span>{title}</span>
+          </div>
+          <div className="preview-contact">
+            <span className="contact-line">
+              <FaPhoneSquareAlt size={15} />
+              {phoneNumber}
+            </span>
+            <span className="contact-line">
+              <MdEmail size={15} />
+              {email}
+            </span>
+            <span className="contact-line">
+              <ImLocation size={15} />
+              {address}
+            </span>
+          </div>
+        </div>
+        <hr />
+        <section>{description}</section>
+      </section>
+      <section className="preview-section">
+        {educationInfo.length > 0 && (
+          <h2 className="preview-name">Education</h2>
+        )}
+        {educationInfo}
+      </section>
+      <section className="preview-section">
+        {workInfo.length > 0 && <h2 className="preview-name">Experiance</h2>}
+        {workInfo}
+      </section>
+    </div>
+  );
+};
+
+export default Preview;
