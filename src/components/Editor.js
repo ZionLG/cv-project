6448e07284c6396af8e8f17f9cf21b7651beabe5
LGGC <liran12369@gmail.com>
2022-10-04 17:00:57 +0300

    add new Editor function component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,27 +3,27 @@ import PersonalDetailEditor from "./PersonalDetailEditor";
 import EducationalExperiance from "./EducationalExperience";
 import WorkExperiance from "./WorkExperience";
 
-export default class Editor extends React.Component {
-  render() {
-    return (
-      <div className="editor">
-        <PersonalDetailEditor
-          personalInfo={this.props.personalInfo}
-          handleOnChange={this.props.handlePersonalOnChange}
-        />
-        <EducationalExperiance
-          addEducation={this.props.addEducation}
-          deleteEducation={this.props.deleteEducation}
-          educationInfo={this.props.educationInfo}
-          handleOnChange={this.props.handleEducationOnChange}
-        />
-        <WorkExperiance
-          workInfo={this.props.workInfo}
-          addWork={this.props.addWork}
-          deleteWork={this.props.deleteWork}
-          handleOnChange={this.props.handleWorkOnChange}
-        />
-      </div>
-    );
-  }
-}
+const Editor = (props) => {
+  return (
+    <div className="editor">
+      <PersonalDetailEditor
+        personalInfo={props.personalInfo}
+        handleOnChange={props.handlePersonalOnChange}
+      />
+      <EducationalExperiance
+        addEducation={props.addEducation}
+        deleteEducation={props.deleteEducation}
+        educationInfo={props.educationInfo}
+        handleOnChange={props.handleEducationOnChange}
+      />
+      <WorkExperiance
+        workInfo={props.workInfo}
+        addWork={props.addWork}
+        deleteWork={props.deleteWork}
+        handleOnChange={props.handleWorkOnChange}
+      />
+    </div>
+  );
+};
+
+export default Editor;
