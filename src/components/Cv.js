a19b1949ec18bb95130b550e9dcb97f77d1b71ba
LGGC <liran12369@gmail.com>
2022-10-05 20:58:07 +0300

    finish transition to hooks and functional components

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,69 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Editor from "./Editor";
 import Preview from "./Preview";
 import uniqid from "uniqid";
 
-export default class Cv extends React.Component {
-  constructor() {
-    super();
+const Cv = () => {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: "",
+    title: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    description: "",
+  });
 
-    this.state = {
-      personalInfo: {
-        fullName: "",
-        title: "",
-        email: "",
-        phoneNumber: "",
-        address: "",
-        description: "",
-      },
-      educationInfo: [],
-      workInfo: [],
-    };
-  }
+  const [educationInfo, setEducationInfo] = useState([]);
+  const [workInfo, setWorkInfo] = useState([]);
 
-  handlePersonalOnChange = (e) => {
+  const handlePersonalOnChange = (e) => {
     const target = e.target;
     const value = target.value;
     const name = target.name;
-    this.setState({
-      personalInfo: {
-        ...this.state.personalInfo,
-        [name]: value,
+    setPersonalInfo({
+      ...personalInfo,
+      [name]: value,
+    });
+  };
+
+  const handleAddEducation = () => {
+    setEducationInfo([
+      ...educationInfo,
+      {
+        id: uniqid(),
+        university: "",
+        degree: "",
+        subject: "",
+        startDate: "",
+        endDate: "",
       },
-    });
+    ]);
   };
 
-  handleAddEducation = () => {
-    this.setState({
-      educationInfo: [
-        ...this.state.educationInfo,
-        {
-          id: uniqid(),
-          university: "",
-          degree: "",
-          subject: "",
-          startDate: "",
-          endDate: "",
-        },
-      ],
-    });
-  };
-
-  handleDeleteEducation = (e) => {
-    let filteredTasks = this.state.educationInfo.filter(
+  const handleDeleteEducation = (e) => {
+    let filteredList = educationInfo.filter(
       (item) => item.id !== e.target.getAttribute("data-id")
     );
 
-    this.setState({
-      educationInfo: filteredTasks,
-    });
+    setEducationInfo(filteredList);
   };
 
-  handleEducationOnChange = (e) => {
-    let objIndex = this.state.educationInfo.findIndex(
+  const handleEducationOnChange = (e) => {
+    let objIndex = educationInfo.findIndex(
       (obj) => obj.id === e.target.parentElement.getAttribute("data-id")
     );
-    let list = this.state.educationInfo;
+    let list = educationInfo;
     const target = e.target;
     const value = target.value;
     const name = target.name;
@@ -72,41 +61,36 @@ export default class Cv extends React.Component {
       ...list[objIndex],
       [name]: value,
     };
-    this.setState({
-      educationInfo: list,
-    });
+
+    setEducationInfo(list);
   };
 
-  handleAddWork = () => {
-    this.setState({
-      workInfo: [
-        ...this.state.workInfo,
-        {
-          id: uniqid(),
-          company: "",
-          position: "",
-          startDate: "",
-          endDate: "",
-        },
-      ],
-    });
+  const handleAddWork = () => {
+    setWorkInfo([
+      ...workInfo,
+      {
+        id: uniqid(),
+        company: "",
+        position: "",
+        startDate: "",
+        endDate: "",
+      },
+    ]);
   };
 
-  handleDeleteWork = (e) => {
-    let filteredTasks = this.state.workInfo.filter(
+  const handleDeleteWork = (e) => {
+    let filteredList = workInfo.filter(
       (item) => item.id !== e.target.getAttribute("data-id")
     );
 
-    this.setState({
-      workInfo: filteredTasks,
-    });
+    setWorkInfo(filteredList);
   };
 
-  handleWorkOnChange = (e) => {
-    let objIndex = this.state.workInfo.findIndex(
+  const handleWorkOnChange = (e) => {
+    let objIndex = workInfo.findIndex(
       (obj) => obj.id === e.target.parentElement.getAttribute("data-id")
     );
-    let list = this.state.workInfo;
+    let list = workInfo;
 
     const target = e.target;
     const value = target.value;
@@ -116,31 +100,30 @@ export default class Cv extends React.Component {
       ...list[objIndex],
       [name]: value,
     };
-    this.setState({
-      workInfo: list,
-    });
+
+    workInfo(list);
   };
 
-  render() {
-    return (
-      <div className="cv">
-        <Editor
-          handlePersonalOnChange={this.handlePersonalOnChange}
-          educationInfo={this.state.educationInfo}
-          addEducation={this.handleAddEducation}
-          deleteEducation={this.handleDeleteEducation}
-          handleEducationOnChange={this.handleEducationOnChange}
-          workInfo={this.state.workInfo}
-          addWork={this.handleAddWork}
-          deleteWork={this.handleDeleteWork}
-          handleWorkOnChange={this.handleWorkOnChange}
-        />
-        <Preview
-          personalInfo={this.state.personalInfo}
-          educationInfo={this.state.educationInfo}
-          workInfo={this.state.workInfo}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="cv">
+      <Editor
+        handlePersonalOnChange={handlePersonalOnChange}
+        educationInfo={educationInfo}
+        addEducation={handleAddEducation}
+        deleteEducation={handleDeleteEducation}
+        handleEducationOnChange={handleEducationOnChange}
+        workInfo={workInfo}
+        addWork={handleAddWork}
+        deleteWork={handleDeleteWork}
+        handleWorkOnChange={handleWorkOnChange}
+      />
+      <Preview
+        personalInfo={personalInfo}
+        educationInfo={educationInfo}
+        workInfo={workInfo}
+      />
+    </div>
+  );
+};
+
+export default Cv;
